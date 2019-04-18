a9fab2dc126fbae082a6521d8757c94751517ba5
galdinojv <joaovictorgaldino.2018@gmail.com>
2019-04-18 10:46:06 -0300

    exer1

---
+++ src/pcg_exer1/execucao.java
@@ -0,0 +1,14 @@
+package pcg_exer1;
+
+import javax.swing.JOptionPane;
+
+	public class execucao {
+	
+		public static void main(String[] args )
+		{
+			pessoa_exer1 ps = new pessoa_exer1() ;
+			ps.andar();
+			ps.respirar();
+		}
+	}
+		
---
+++ src/pcg_exer1/pessoa_exer1.java
@@ -0,0 +1,17 @@
+package pcg_exer1;
+
+import javax.swing.JOptionPane;
+
+public class pessoa_exer1 {
+
+	public void andar() {
+		JOptionPane.showMessageDialog(null,"Andando");
+	}
+	protected void respirar() {
+		JOptionPane.showMessageDialog(null,"Respirando");
+	}
+	private void dormir() {
+		JOptionPane.showMessageDialog(null,"Dormindo");
+	}
+	
+}
